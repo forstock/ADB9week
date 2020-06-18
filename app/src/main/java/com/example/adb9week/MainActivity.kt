4e4eb0d61261a54744a60c77ce80bef555ab1bce
forstock <forstock@hotmail.com>
2020-06-17 19:49:35 -0600

    week2

--- app/src/main/java/com/example/adb9week/MainActivity.kt
+++ app/src/main/java/com/example/adb9week/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.adb9week
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val zipCodeEditText: EditText = findViewById(R.id.editText)
+        val submitButton: Button = findViewById(R.id.submitButton)
+        submitButton.setOnClickListener {
+            var zipcode: String =editText.text.toString()
+            if (zipcode.length !=5) {
+                Toast.makeText(this,"Invalid zipcode", Toast.LENGTH_LONG).show()
+
+            } else {
+                Toast.makeText(this, zipcode, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     override fun onStart() {
